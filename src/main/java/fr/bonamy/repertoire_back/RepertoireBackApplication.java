ea96b3e7a0c6fac690440926dbb86d18d584e855
Antoine Bonamy <antoine.bonamy2@gmail.com>
2023-11-30 22:41:20 +0100

    Initial commit

---
+++ src/main/java/fr/bonamy/repertoire_back/RepertoireBackApplication.java
@@ -0,0 +1,13 @@
+package fr.bonamy.repertoire_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepertoireBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepertoireBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/bonamy/repertoire_back/RepertoireBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bonamy.repertoire_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepertoireBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
