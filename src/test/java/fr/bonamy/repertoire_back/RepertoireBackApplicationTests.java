c32276812b132efb8e28a75f12afb90bfc65784f
Antoine Bonamy <antoine.bonamy2@gmail.com>
2024-05-04 03:05:30 +0200

    Remove @SpringBootTest

--- src/test/java/fr/bonamy/repertoire_back/RepertoireBackApplicationTests.java
+++ src/test/java/fr/bonamy/repertoire_back/RepertoireBackApplicationTests.java
@@ -1,9 +1,7 @@
 package fr.bonamy.repertoire_back;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RepertoireBackApplicationTests {
 
 	@Test
