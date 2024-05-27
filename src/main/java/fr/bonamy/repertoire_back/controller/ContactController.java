11cb9fad079a8c373a2dc54d78358fbb596d44e8
Antoine Bonamy <antoine.bonamy2@gmail.com>
2024-05-28 01:40:44 +0200

    Fix test

--- src/main/java/fr/bonamy/repertoire_back/controller/ContactController.java
+++ src/main/java/fr/bonamy/repertoire_back/controller/ContactController.java
@@ -7,12 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/contacts")
 public class ContactController {
 
